3ed8ae0c6ef15a24626fc86fe2dcdaa332fdfb53
Graeme Harvey <graeme@mappedin.ca>
2018-11-01 13:47:10 -0400

    tweaking urls and statuses

--- src/dont-merge.js
+++ src/dont-merge.js
@@ -6,12 +6,12 @@ function handleRequestLabels (robot, context) {
     const {labels, head} = context.payload.pull_request;
     const hasDontMergeLabel = labels.some(label => utils.isDontMerge(label.name));
     const state = hasDontMergeLabel ? 'failure' : 'success';
-    const description = hasDontMergeLabel ? 'QA before merging.' : 'Ready for review/merge';
+    const description = hasDontMergeLabel ? 'Please QA before merging!' : 'QA good to go!';
 
     context.github.repos.createStatus(context.repo({
       sha: head.sha,
-      target_url: 'https://github.com/apps/github-needs-qa',
-      context: 'github-needs-qa',
+      target_url: 'https://github.com/apps/block-on-qa',
+      context: 'Block on QA',
       state,
       description
     }));
